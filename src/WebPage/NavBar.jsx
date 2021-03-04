d9b53f6ece411c3edaa2374a793ae6534e5e918c
mitzanu <mitzanu@hotmail.com>
2021-03-04 22:51:25 +0200

    update

--- src/WebPage/NavBar.jsx
+++ src/WebPage/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
                     {/* <Nav.Link href="/about">About Me</Nav.Link> */}
                     <NavDropdown title="My Applications" id="collasible-nav-dropdown">
                         <NavDropdown.Item href="https://mitz-ballgame.netlify.app">BallGame</NavDropdown.Item>
-                        <NavDropdown.Item href="/Chess">Chess</NavDropdown.Item>
+                        <NavDropdown.Item href="https://mitz-invoice-app.netlify.app/">InvoiceApp</NavDropdown.Item>
                         <NavDropdown.Item href="Something/3.3">Something</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
